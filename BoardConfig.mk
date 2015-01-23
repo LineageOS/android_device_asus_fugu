a3667170d3a725f4ab42a89b2e2cc6ff9dfa53f2
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-01-22 18:59:25 -0800

    cm-14.0 changes for Nexus Player (fugu)
    * Replace AOSP kernel build with CyanogenMod style.
    * Replace blob extractions with generics
    * Replace toolchain with androidkernel
    * Remove deprecated vendorsetup.sh
    
    Change-Id: I707c83f7128fcfd6d372053c94a2cae03d9d4026

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,13 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 MALLOC_SVELTE := true
 
+# Inline kernel building
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-androidkernel-
+TARGET_KERNEL_CONFIG := fugu_defconfig
+TARGET_KERNEL_SOURCE := kernel/asus/fugu
+TARGET_KERNEL_ARCH := x86_64
+BOARD_KERNEL_IMAGE_NAME := bzImage
+
 # Kernel cmdline
 BOARD_KERNEL_CMDLINE := pci=noearly vmalloc=256M ptrace.ptrace_can_access=1
 BOARD_KERNEL_CMDLINE += earlyprintk=nologger loglevel=8
