9ed4d435acfb5a8fc2a173f48d03f125a9654655
dhacker29 <dhackerdvm@gmail.com>
2015-04-09 15:37:03 -0400

    fugu: Report ARM ABI'sto match stock
    * This is for Houdini (native arm32 translation).
    
    Change-Id: I22e18064451074988b46faeaa7b6bf21b03244fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ DEVICE_PATH := device/asus/fugu
 TARGET_ARCH := x86
 TARGET_ARCH_VARIANT := silvermont
 TARGET_CPU_ABI := x86
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_CPU_ABI_LIST := x86,armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
 
 ## Audio
 BOARD_USES_ALSA_AUDIO := true
