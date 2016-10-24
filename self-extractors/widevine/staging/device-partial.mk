ca18428837d6c80cb566d1ee2eaf551bbb512990
Jeff Tinker <jtinker@google.com>
2016-10-24 13:51:14 -0700

    Removing widevine classic
    bug:30505334
    Change-Id: I4924bb68fab03a6111cfc413562f94d3cb0294cf

--- self-extractors/widevine/staging/device-partial.mk
+++ self-extractors/widevine/staging/device-partial.mk
@@ -12,16 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Widevine blob(s) necessary for Fugu hardware
-PRODUCT_COPY_FILES := \
-    vendor/widevine/fugu/proprietary/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml:widevine \
-    vendor/widevine/fugu/proprietary/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar:widevine \
     vendor/widevine/fugu/proprietary/libdrmframework.so:system/lib/arm/libdrmframework.so:widevine \
-    vendor/widevine/fugu/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so:widevine \
-    vendor/widevine/fugu/proprietary/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so:widevine \
     vendor/widevine/fugu/proprietary/liboemcrypto.so:system/vendor/lib/liboemcrypto.so:widevine \
-    vendor/widevine/fugu/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so:widevine \
-    vendor/widevine/fugu/proprietary/libwvm.so:system/vendor/lib/libwvm.so:widevine \
-    vendor/widevine/fugu/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so:widevine \
     vendor/widevine/fugu/proprietary/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:widevine \
-
