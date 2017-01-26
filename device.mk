e659cd90d76e7c9a7bd99a6a19036733e7715663
Nick Desaulniers <ndesaulniers@google.com>
2017-01-26 12:31:52 -0800

    fugu: stop dex preopt'ing
    Instead of selectively dex preopt'ing apps, don't do so for any apps.
    
    Bug: 34637939
    Bug: 34692070
    Bug: 34732774
    Test: rm -rf $OUT/system; make;
    Change-Id: I1d3f278029c71e09f008aee2d452c0ffd5516b5c

--- device.mk
+++ device.mk
@@ -331,28 +331,8 @@ PRODUCT_COPY_FILES += \
     device/asus/fugu/init.fugu.countrycode.sh:system/bin/init.fugu.countrycode.sh
 
 # Get rid of dex preoptimization to save space within system.img at the one
-# time cost of dexing on first boot.  This list can be found by running:
-# grep odex $ANDROID_PRODUCT_OUT/installed-files.txt
-# Sorted by *.odex size
-FUGU_DONT_DEXPREOPT_MODULES := \
-    NoTouchAuthDelegate \
-    RemoteControlService \
-    SystemUI \
-    ConfigUpdater \
-    Bluetooth \
-    SecondScreenSetup \
-    SecondScreenSetupAuthBridge \
-    TvSettings \
-    SetupWraith \
-    GooglePackageInstaller \
-    DownloadProvider \
-    GoogleContactsSyncAdapter \
-    BugReportSender \
-    ContactsProvider \
-    PrintSpooler \
-    Overscan \
-
-$(call add-product-dex-preopt-module-config,$(FUGU_DONT_DEXPREOPT_MODULES),disable)
+# time cost of dexing on first boot.
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
 
 # Some CTS tests will be skipped based on what the initial API level that
 # shipped on device was.
