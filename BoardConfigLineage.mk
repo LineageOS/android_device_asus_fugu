a9d9fc497b88d73fe865e0b07c08278a3b4403d3
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-27 13:08:58 -0500

    fugu: build kernel inline
    Change-Id: Id9b0f6560028b940c61ce1d70796cf89a9e7292e

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,1 +1,8 @@
 # Common board config for fugu
+
+# Kernel
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
+TARGET_KERNEL_CONFIG := lineage_fugu_defconfig
+TARGET_KERNEL_SOURCE := kernel/asus/fugu
+TARGET_KERNEL_ARCH := x86_64
+BOARD_KERNEL_IMAGE_NAME := bzImage
