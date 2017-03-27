274ae5745a9be392a1c4f0072198d4415b219b39
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-27 13:14:13 -0500

    fugu: add proprietary blobs
    Change-Id: I327f8c8aa4cc3a42e38ca17cd5af79d5ff384792

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,3 +6,6 @@ TARGET_KERNEL_CONFIG := lineage_fugu_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/fugu
 TARGET_KERNEL_ARCH := x86_64
 BOARD_KERNEL_IMAGE_NAME := bzImage
+
+# Vendor blobs
+-include vendor/google/marlin/BoardConfigVendor.mk
