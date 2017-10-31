b868033cdeadac4c77ec28b7e962bd49c21c1602
Chih-Hung Hsieh <chh@google.com>
2017-10-31 14:57:21 -0700

    Use -Werror in device/asus/fugu
    * Suppress existing non-critical warnings.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I90177677f15d5c9ef4ce4f0d3a81e50873982f71

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -6,6 +6,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := librecovery_updater_fugu
 LOCAL_SRC_FILES := recovery_updater.cpp fw_version_check.cpp
+LOCAL_CFLAGS := -Wall -Werror
+# 'ErrorAbort' is deprecated
+LOCAL_CFLAGS += -Wno-deprecated-declarations
+# 'readbyte_umip_emmc' is unused
+LOCAL_CFLAGS += -Wno-unused-function
 LOCAL_STATIC_LIBRARIES := libedify
 
 include $(BUILD_STATIC_LIBRARY)
@@ -15,6 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += bootable/recovery
 LOCAL_SRC_FILES := recovery_ui.cpp
+LOCAL_CFLAGS := -Wall -Werror
 
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
 LOCAL_MODULE := librecovery_ui_fugu
