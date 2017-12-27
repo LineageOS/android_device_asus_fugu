02c023e215ccd7ec8f9ba190b8304ab3e0711d91
Nolen Johnson <johnsonnolen@gmail.com>
2017-12-27 15:59:06 -0500

    fugu: Advertise that we have native ntfs support in kernel
    Change-Id: If9d949f7ab3a25229c4de08c91a448876dafa8c6

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,6 +6,6 @@ TARGET_KERNEL_CONFIG := lineage_fugu_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/fugu
 TARGET_KERNEL_ARCH := x86_64
 BOARD_KERNEL_IMAGE_NAME := bzImage
-
+TARGET_KERNEL_HAVE_NTFS := true
 # Vendor blobs
 -include vendor/asus/fugu/BoardConfigVendor.mk
