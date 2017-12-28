f3906a367d85bbd81300a58b3c9e96ff1874590b
dhacker29 <dhackerdvm@gmail.com>
2017-12-28 17:16:59 -0500

    fugu: Advertise that we have native exfat driver
    Change-Id: I560f2e5e9bd625e8c49eb5ed8b8cfc3f541d252c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,6 +6,8 @@ TARGET_KERNEL_CONFIG := lineage_fugu_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/fugu
 TARGET_KERNEL_ARCH := x86_64
 BOARD_KERNEL_IMAGE_NAME := bzImage
+TARGET_KERNEL_HAVE_EXFAT := true
 TARGET_KERNEL_HAVE_NTFS := true
+
 # Vendor blobs
 -include vendor/asus/fugu/BoardConfigVendor.mk
