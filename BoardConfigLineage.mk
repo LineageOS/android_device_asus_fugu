4935f5a7da368cee31002798ca11e5fbd23ebb64
Ethan Yonker <dees_troy@teamw.in>
2017-12-31 19:35:01 -0500

    fugu: Add TWRP Support
    To build:
    
      * Clone https://github.com/omnirom/android_bootable_recovery/
        to bootable/recovery-twrp (make sure to use the corresponding Android version branch, i.e. android-8.1 for lineage-15.1)
    
      * (Optional) If you wish to use BusyBox for wider tool compatibllity, clone a buildable and functional Android 8.1 busybox
        to external/busybox, or use a prebuilt and add the corresponding PRODUCT_COPY_FILE to /system/xbin/ where TWRP expects it, then comment TW_USE_TOOLBOX in twrp.mk. -- I opted to use ToolBox by defualt because I couldn't find a buildable/functional busybox for 8.1 trees (they all fail to build, including LineageOS's repo, which wasn't brought up to lineage-15.1).
    
      * WITH_TWRP=true mka recoveryimage
    
    Change-Id: Ie5ee23d020c8fa7642658ea957c86cb5c1818b6c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,4 +1,4 @@
-# Common board config for fugu
+# Lineage board config for fugu
 
 # Kernel
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
@@ -6,6 +6,11 @@ TARGET_KERNEL_CONFIG := lineage_fugu_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/fugu
 TARGET_KERNEL_ARCH := x86_64
 BOARD_KERNEL_IMAGE_NAME := bzImage
+
+#TWRP (optional)
+ifeq ($(WITH_TWRP),true)
+-include device/asus/fugu/twrp.mk
+endif
 
 # Vendor blobs
 -include vendor/asus/fugu/BoardConfigVendor.mk
