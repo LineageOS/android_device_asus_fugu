05db89cb1dbbd89c11f9f0ba4c1138d1549f43ad
Nolen Johnson <johnsonnolen@gmail.com>
2018-04-02 20:24:49 -0400

    fugu: Fix vendor image mismatch
    * Needed after https://review.lineageos.org/#/c/208868/
    
    Change-Id: Id2e06ba25f8ba45a8574fb91d78fb752db808942

--- lineage.mk
+++ lineage.mk
@@ -16,8 +16,9 @@ PRODUCT_MANUFACTURER := Asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=fugu \
-    BUILD_FINGERPRINT=google/fugu/fugu:8.0.0/OPR2.170623.027/4397545:user/release-keys \
     PRIVATE_BUILD_DESC="fugu-user 8.0.0 OPR2.170623.027 4397545 release-keys"
+
+BUILD_FINGERPRINT := google/fugu/fugu:8.0.0/OPR2.170623.027/4397545:user/release-keys
 
 $(call inherit-product, vendor/asus/fugu/fugu-vendor.mk)
 $(call inherit-product, vendor/google/atv/atv-common.mk)
