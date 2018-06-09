ba92b7055ac2ddc18028c103e3a578e34fde3156
Nolen Johnson <johnsonnolen@gmail.com>
2018-06-09 18:26:13 -0400

    fugu: Remove apps that are now common
    * Introduced in https://github.com/LineageOS/android_vendor_lineage/commit/30768b82c17cb75bf25e62b5979dba4c925d7eef.
    
    * LineageCustomize was dropped as of 8.0.0, as we never got a newer partner example for the new launcher.
    
    Change-Id: Ia27bdb81c40f6ffd151b1eb4ab850704151c1b95

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,7 +1,3 @@
-PRODUCT_PACKAGES += \
-    AppDrawer \
-    LeanbackCustomize
-
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2017-11-05
