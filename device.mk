d773f5dcb98581d0191a79cbbb72b4b12a4fa132
Bruno Martins <bgcngm@gmail.com>
2019-03-12 05:46:17 -0400

    fugu: Build the common basic USB HAL
    * Also add to the manifest.
    
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -101,6 +101,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gpu/powervr.ini:system/etc/powervr.ini
 
+## USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 ## HDMI-CEC
 PRODUCT_PACKAGES += \
     android.hardware.tv.cec@1.0-impl
