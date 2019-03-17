8f2d8ac3cf1bbaf3c3653c2fdb32cc1b429ed7e1
Nolen Johnson <johnsonnolen@gmail.com>
2019-03-17 05:08:30 +0000

    [DNM] fugu: Set SELinux to Permissive
    * For debugging/bring up purposes only.
    
    Change-Id: I4723dc3f1aed6d0ecc42f53d44ac0291c52a5552

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=fugu androidboot.serialno=012345678
 BOARD_KERNEL_CMDLINE += snd_pcm.maximum_substreams=8
 BOARD_KERNEL_CMDLINE += intel_soc_pmu.enable_s3=0
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Binder API version
 TARGET_USES_64_BIT_BINDER := true
