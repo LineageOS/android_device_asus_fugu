0334e90f497f5a72e13256d175be7bc2ab5ab5e8
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-06 23:10:30 -0400

    fugu: Move to device/lineage/atv
    Change-Id: Iff2513dffa634bad00875c8f841de70d898e0f6b
    (cherry picked from commit 0de370ccf744fdede184a1640e8eae3c9222f368)

---
+++ lineage.mk
@@ -0,0 +1,28 @@
+<<<<<<< HEAD   (4f7114 fugu: Build audiopolicymanager.fugu)
+=======
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 1920
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/fugu/aosp_fugu.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_fugu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus Player
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=fugu \
+    PRIVATE_BUILD_DESC="fugu-user 8.0.0 OPR2.170623.027 4397545 release-keys"
+
+BUILD_FINGERPRINT := google/fugu/fugu:8.0.0/OPR2.170623.027/4397545:user/release-keys
+
+$(call inherit-product, vendor/asus/fugu/fugu-vendor.mk)
+$(call inherit-product, device/lineage/atv/lineage_atv.mk)
+
+>>>>>>> CHANGE (0de370 fugu: Move to device/lineage/atv)
